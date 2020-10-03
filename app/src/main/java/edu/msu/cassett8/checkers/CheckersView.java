91a489338fb5444b95df29029fcb113d4a5f867d
sdcas <sdcas@DESKTOP-NLTDQQL>
2020-10-03 14:11:04 -0400

    Added drawable resources, checkersview for the game board, and made my layout.

---
+++ app/src/main/java/edu/msu/cassett8/checkers/CheckersView.java
@@ -0,0 +1,63 @@
+package edu.msu.cassett8.checkers;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class CheckersView extends View {
+
+    private TextPaint mTextPaint;
+    private float mTextWidth;
+    private float mTextHeight;
+
+    public CheckersView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public CheckersView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public CheckersView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        setBackgroundResource(R.drawable.checkerboard);
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.CheckersView, defStyle, 0);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        // Draw the text.
+    }
+
+}
